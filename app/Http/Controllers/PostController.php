bc4380ea4007fbd049fe562b2295810796e146c1
tsuhia <hikahika1109@icloud.com>
2021-04-08 14:31:22 +0900

    [add]課題8-4途中

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,20 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('index')->with(['posts' => $post->get()]);
+    }
+    public function show(Post $post)
+    {
+        return view('show')->with(['posts' => $post]);
+    }
+    public function create()
+    {
+        return view('create');
+    }
+    public function store(Request $request, Post $post)
+    {
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
     }
 }
