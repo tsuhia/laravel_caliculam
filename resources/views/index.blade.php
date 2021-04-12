a3ddbd14655eb0a229ed482abf2f90a607b11c84
tsuhia <hikahika1109@icloud.com>
2021-04-12 15:12:26 +0900

    適切なコミットメント

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,20 +3,17 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
+        
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet"
-    ></head>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    </head>
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
-            @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
-                    <p class='body'>{{ $post->body }}</p>
+                    <h2 class='title'>Title 1</h2>
+                    <p class='body'>This is a sample body.</p>
                 </div>
-            @endforeach
         </div>
-        
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/posts', 'PostController@index');
