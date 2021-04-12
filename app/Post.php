5d44a62e95d99779ecec0177507ad3d63b1ca177
tsuhia <hikahika1109@icloud.com>
2021-04-12 14:38:46 +0900

    [add]課題8-4途中

--- app/Post.php
+++ app/Post.php
@@ -11,6 +11,12 @@ class Post extends Model
         'body' ,
     ];
     
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count);
+    }
+    
     public function get()
     {
         return $this->orderBy('updated_at' , 'DESC')->paginate(5);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <a href='/posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
+                    <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
