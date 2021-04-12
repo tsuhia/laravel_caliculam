242630e3036c8a0696e94adf01b6a58e6ab95086
tsuhia <hikahika1109@icloud.com>
2021-04-12 17:41:45 +0900

    課題８-3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <a href='/posts/{{ post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
+                    <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,5 +13,5 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
 Route::get('/posts/{post}', 'PostController@show');
