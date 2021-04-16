1e7d6f57996382d64eb556e9dcc73529c77f81cd
tsuhia <hikahika1109@icloud.com>
2021-04-16 17:00:23 +0900

    課題８-5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
 
+        return view('index')->with(['posts' => $post->getPaginate()]);
+    }
+    public function show(Post $post)
+    {
+        return view('show')->with(['posts' => $post]);
+    }
+    public function create()
+    {
+        return view('create');
+    }
+    public function store(Post $post , PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+    }
 }
 ?>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
+Route::get('/posts/{post}', 'PostController@show');
+
+Route::post('/posts', 'PostController@store');
